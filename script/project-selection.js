cdcf4261c330fcb3a7ceabc9e6a883195933b3fa
accademiafacile <info@vitoesposito.it>
2023-01-30 03:39:40 +0100

    copy adjustments and content transcrption

--- script/project-selection.js
+++ script/project-selection.js
@@ -1,3 +1,20 @@
+// const projects = document.querySelectorAll('.project');
+// const projectInfos = document.querySelectorAll('.project-info');
+
+// projects.forEach(function(project, index) {
+//   project.addEventListener('click', function() {
+//     projects.forEach(function(project) {
+//       project.classList.remove('active');
+//     });
+//     project.classList.add('active');
+//     projectInfos.forEach(function(projectInfo) {
+//       projectInfo.classList.add('hide');
+//     });
+//     projectInfos[index].classList.remove('hide');
+//   });
+// });
+
+
 const projects = document.querySelectorAll('.project');
 const projectInfos = document.querySelectorAll('.project-info');
 
@@ -5,7 +22,9 @@ projects.forEach(function(project, index) {
   project.addEventListener('click', function() {
     projects.forEach(function(project) {
       project.classList.remove('active');
+      project.classList.add('idle');
     });
+    project.classList.remove('idle');
     project.classList.add('active');
     projectInfos.forEach(function(projectInfo) {
       projectInfo.classList.add('hide');
